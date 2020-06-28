2822a7607fed4029c4a5f3b6270fa54dfc160802
niloccolin <ce59642@gmail.com>
2020-06-28 14:07:59 -0700

    added functions and variables to .js file and used console and console.log.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?"); //assign var playerName to window prompt "what is ur robot's name?"
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+//this will do math and log 20
+console.log(10 + 10);
+
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+window.alert("This is an alert! JavaScript is running!"); //window alert "JS is running"
+
+function fight() {
+    window.alert("The fight has begun!"); //window alert "the fight has begun"
+}
+
+// fight();
