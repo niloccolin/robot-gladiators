b236335da83c2af6454555e602df5658c8b645b9
niloccolin <ce59642@gmail.com>
2020-07-06 15:20:15 -0700

    mmm

--- assets/js/game.js
+++ assets/js/game.js
@@ -63,14 +63,31 @@
             // FIGHT or SKIP prompt
             var promptFight = window.prompt("Do you want to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
+            if (promptFight === "skip" || promptFight === "SKIP") {
+    
+                // confirm if you really want to quit
+                var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+                
+                // if yes (true), leave fight
+                if (confirmSkip) {
+                    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                    // subtract money from playerMoney for skipping
+                    playerMoney = playerMoney - 10;
+                    console.log("playerMoney", playerMoney);
+                    break;
+                }
+                // if no (false), ask question again by running fight() again
+                else {
+                    fight();
+                }
+            }
+            else if (promptFight === "fight" || promptFight === "FIGHT") {
 
-            if (promptFight === "fight" || "FIGHT") {
 
-
-                // damage enemy by subtacting the amount set in the <playerAttack> variable
+                // damage enemy by subtracting the amount set in the <playerAttack> variable
                 enemyHealth = enemyHealth - playerAttack;
                 console.log(
-                    playerName + " attacked " + enemyNames[i] + ". " + enemyNames[i] + " now has " + enemyHealth + " health remaining."
+                    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
                 )
 
 
@@ -78,7 +95,7 @@
                 // from damage taken by enemy by subtracting the amount set in the <enemyAttack> variable
                 playerHealth = playerHealth - enemyAttack;
                 console.log(
-                    enemyNames[i] + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+                    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
                 );
 
 
@@ -93,45 +110,18 @@
 
                 // enemy's health stat
                 if (enemyHealth <= 0) {
-                    window.alert(enemyNames[i] + " has died!");
+                    window.alert(enemyNames + " has died!");
                     fight();
-                } else {
-                    window.alert(enemyNames[i] + " still has " + enemyHealth + " health left.");
                 }
-
-
-            }
-
-
-            if (promptFight === "skip" || promptFight === "SKIP") {
-    
-                // confirm if you really want to quit
-                var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-                
-                // if yes (true), leave fight
-                if (confirmSkip) {
-                    window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                    // subtract money from playerMoney for skipping
-                    playerMoney = playerMoney - 10;
-                    console.log("playerMoney", playerMoney);
-                    break;
-                }
-                    
-                // if no (false), ask question again by running fight() again
                 else {
-                    fight();
+                    window.alert(enemyNames + " still has " + enemyHealth + " health left.");
                 }
-
             }
-            
-
             else {
                 window.alert("You need to pick a valid option. Try again!");
             }
-
         }
-    }
-
+    }      
 
     // startGame function
     var startGame = function() {
@@ -142,18 +132,28 @@
 
         // enemyNames array iterator
         for (var i = 0; i < enemyNames.length; i ++) {
+            
             if (playerHealth > 0) {
                 window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
+                
                 var pickedEnemyName = enemyNames[i];
 
                 enemyHealth = 50;
 
                 fight(pickedEnemyName);
-
             }
-            else {
-                window.alert("You have lost your robot in battle! Game Over!");
+            // if we're not at the last enemy in the array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                    // ask if user wants to use the store before next round
+                    var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                    // if yes, take them to the store() function
+                    if (storeConfirm) {
+                    shop();
+                }
+            }
+            else if (playerHealth <= 0) {
+                window.alert("You have lost your robot in battle! Game over!");
                 break;
             }
 
@@ -164,7 +164,7 @@
     
     }
 
-    var endGame = function() {
+    function endGame() {
         //if the player is still alive, player wins!
         if (playerHealth > 0) {
             window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
@@ -183,8 +183,59 @@
         else {
             window.alert("Thank you for playing Robot Gladiators! Come back soon!");
         }
+
     };
 
+    function shop() {
+        // ask the player what they'd like to do
+        var shopOptionPrompt = window.prompt(
+            "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        );
+
+        // use switch to carry out action
+        switch (shopOptionPrompt) {
+            case "REFILL": // new case
+            case "refill":
+                if (playerMoney >= 7) {
+                window.alert("Refilling player's health by 20 for 7 dollars.");
+
+                // increase health and decrease money
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+                }
+                else {
+                    window.alert("You don't have enough money!");
+                }
+
+                break;
+            case "UPGRADE": // new case
+            case "upgrade":
+                if (playerMoney >= 7) {
+                window.alert("Upgrading player's attack by 6 for 7 dollars.");
+            
+                // increase attack and decrease money
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+                }
+                else {
+                    window.alert("You don't have enough money!");
+                }
+
+                break;
+            case "LEAVE": // new case
+            case "leave":
+                window.alert("Leaving the store.");
+
+                // do nothing, so function will end
+                break;
+            default:
+                window.alert("You did not pick a valid option. Try again.");
+
+                //call shop() again to force player to pick a valid option
+                shop();
+                break;
+        }
+    };
 
 
 startGame();
