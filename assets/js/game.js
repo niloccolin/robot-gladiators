3ace57aa2bc4e202340bb4cb4cb0160de59b1d01
niloccolin <ce59642@gmail.com>
2020-07-07 15:11:16 -0700

    playerInfo.name bug fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -215,10 +215,20 @@
         };
     };
 
+    var getPlayerName = function() {
+        var name = "";
+
+        while(name === "" || name === null) {
+            name = prompt("What is your robot's name?");
+        }
+
+        console.log("Your robot's name is " + name );
+        return name;
+    }
 
     // player stats
     var playerInfo = {
-        name: window.prompt("What is your robot's name?"),
+        name: getPlayerName(),
         health: 100,
         attack: 10,
         money: 10,
